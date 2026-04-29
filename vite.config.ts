16c3e44bdc68d686c1d4f56eb36f8eca2d97bfec
PSW <tjddnr633@naver.com>
2026-04-29 18:03:17 +0900

    [-] nc 링크 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath } from 'node:url'
+
+const indexHtml = fileURLToPath(new URL('./index.html', import.meta.url))
+const ncHtml = fileURLToPath(new URL('./nc.html', import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: indexHtml,
+        nc: ncHtml,
+      },
+    },
+  },
 })
